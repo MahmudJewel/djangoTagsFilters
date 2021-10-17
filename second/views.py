d3b67834deccf69dd9733bf3fae1a2eebd92c740
jewel <jmbengal.mbstu@gmail.com>
2021-10-17 16:19:12 +0600

    some changes

--- second/views.py
+++ second/views.py
@@ -6,7 +6,14 @@ def home(request):
     return render(request,'home.html')
 
 def lower(request):
-    return render(request,'BuiltInTags/lower.html')
+    txt=''
+    if request.method=='POST':
+        txt=request.POST.get('tarea')
+        #print(txt)
+    context={
+            'txt':txt,
+        }
+    return render(request,'BuiltInTags/lower.html', context)
     
 def reference(request):
     return render(request,'BuiltInTags/Reference.html')
